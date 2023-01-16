b568ed1779ba9eb69eb960b59d68f3f598ccf7bd
juliancasali <juliancasalii@gmail.com>
2023-01-16 20:59:45 -0300

    22. Persistindo entidades com EntityManager

--- src/main/java/com/example/springbootvendas/repository/Clientes.java
+++ src/main/java/com/example/springbootvendas/repository/Clientes.java
@@ -1,10 +1,12 @@
 package com.example.springbootvendas.repository;
 
 import com.example.springbootvendas.entity.Cliente;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -22,8 +24,11 @@ public class Clientes {
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
+    private EntityManager entityManager;
+
+    @Transactional
     public Cliente save(Cliente cliente){
-        jdbcTemplate.update(INSERT, cliente.getName());
+        entityManager.persist(cliente);
         return cliente;
     }
 
