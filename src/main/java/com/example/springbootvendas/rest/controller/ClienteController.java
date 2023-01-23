049814027f4131c9c2d69cb69557fa9a05236cc8
juliancasali <juliancasalii@gmail.com>
2023-01-22 22:30:44 -0300

    38. Requisição GET com parâmetros e Response Entity

---
+++ src/main/java/com/example/springbootvendas/rest/controller/ClienteController.java
@@ -0,0 +1,27 @@
+package com.example.springbootvendas.rest.controller;
+
+import com.example.springbootvendas.domain.entity.Cliente;
+import com.example.springbootvendas.domain.repository.Clientes;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
+
+@Controller
+public class ClienteController {
+
+    private final Clientes clientes;
+
+    public ClienteController(Clientes clientes) {
+        this.clientes = clientes;
+    }
+
+    @GetMapping("/api/clientes/{id}")
+    @ResponseBody
+    public ResponseEntity<Cliente> getClienteById(@PathVariable Integer id) {
+        Optional<Cliente>  cliente = clientes.findById(id);
+        return cliente.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+}
